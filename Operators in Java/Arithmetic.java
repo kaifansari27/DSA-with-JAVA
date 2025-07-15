d2116fae246af4a7e31b1bb115341a03f3ec270d
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 17:44:08 +0530

    operators

---
+++ Operators in Java/Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic {
+    public static void main (String [] args){
+        int A = 20;
+        int B = 10;
+        System.out.println("Add = " + (A+B));
+        System.out.println("sub = " + (A-B));
+        System.out.println("Multi = " + (A*B));
+        System.out.println("Div = " + (A/B));
+        System.out.println("Rem = " + (A%B));
+    }
+}
