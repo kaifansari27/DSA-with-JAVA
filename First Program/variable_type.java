95530cc883b61dfb288d4c3e751d69223b9024e3
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 13:32:32 +0530

    variable type

---
+++ First Program/variable_type.java
@@ -0,0 +1,13 @@
+public class variable_type {
+    public static void main(String[] args) {
+        byte b = 9;
+        System.out.println(b);
+        char ch = 'K';
+        System.out.println(ch);
+        boolean var = true;
+        System.out.println(var);
+        int number =  27 ;
+        System.out.println(number);
+    }
+    
+}
