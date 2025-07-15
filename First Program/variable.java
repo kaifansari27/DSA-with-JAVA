44b1126735a7702d79e615ab13dd9999bea4e4f5
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 12:46:22 +0530

    Variables

---
+++ First Program/variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = 20 ;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "kaif Ansari";
+        System.out.println(name);
+        a = 100;
+        System.out.println(a);
+    }
+}
