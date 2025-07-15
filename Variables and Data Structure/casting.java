056c2024ac57b0012607ef96280b52f398ba9cbb
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 17:00:43 +0530

    castin -> forcefully conversion

---
+++ Variables and Data Structure/casting.java
@@ -0,0 +1,9 @@
+public class casting {
+    public static void main (String [] args){
+        float a = 25.999999f ;
+        int b = (int) a ;
+        System.out.println(b);
+    
+    }
+    
+}
---
+++ Variables and Data Structure/promotion.java
@@ -0,0 +1,32 @@
+/*public class promotion {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int)(a));
+        System.out.println((int)(b));
+        System.out.println(b-a);
+    }
+}*/
+
+/*public class promotion{
+    public static void main(String[] args) {
+        short a = 5 ;
+        byte b = 25 ;
+        char c = 'c';
+        byte bt = (byte) (a+b+c);
+        System.out.println(bt);
+    }
+}*/
+
+/*public class promotion{
+    public static void main(String[] args) {
+        int a =  10 ;
+        float b = 25.20f;
+        long c = 30;
+        double d = 50;
+        double ans = a + b + c + d ;
+        System.out.println(ans);
+
+
+    }
+}*/
