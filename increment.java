1ee93342179a072baab85eee26b4c29f4d21e633
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 18:16:37 +0530

    increments

---
+++ increment.java
@@ -0,0 +1,20 @@
+// pre increment
+/*public class increment {
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}*/
+
+//post increment
+
+public class increment{
+    public static void main(String[] args) {
+        int a = 10 ;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
