4bb5663c23c9e1fb534f7f051206ae49e786d417
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-15 14:26:15 +0530

    sum of two number

---
+++ sum_ab.java
@@ -0,0 +1,9 @@
+public class sum_ab {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 17;
+        int sum  = a+b;
+        System.out.println(sum);
+    }
+    
+}
