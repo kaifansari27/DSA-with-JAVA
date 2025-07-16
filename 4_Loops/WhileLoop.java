c6dd7f37cb99c57730913d2266c18b21a3d7f76f
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 18:07:23 +0530

    While loop

---
+++ 4_Loops/WhileLoop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class WhileLoop {
+    public static void main(String[] args) {
+        int counter = 0 ;
+        while (counter<100) {
+            System.out.println("kaif Ansari");
+            counter ++;
+            
+        }
+        System.out.println("Printed HW 100x");
+    }
+}
