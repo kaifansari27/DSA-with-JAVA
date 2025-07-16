c417e806bfa8c4d1e3c3a10b6af24b3591e6eca0
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 17:32:07 +0530

    test ka solution

---
+++ 3_Conditional Statements/test.java
@@ -0,0 +1,11 @@
+
+public class test {
+    public static void main(String[] args) {
+        int a = 63;
+        int b = 36;
+        String type = (a < b) ? "true" : "false" ;
+        System.out.println(type);
+        int number = (a>b) ? a : b ;
+        System.out.println(number);
+    }
+}
