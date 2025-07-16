1b685cfaeaeec574be62f31cb17a7699a41859b5
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 10:01:43 +0530

    Logical Operators

---
+++ 2_Operators in Java/logicalOperators.java
@@ -0,0 +1,7 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+        System.out.println( (3>2) && (10<20 ));
+        System.out.println( (3>2) || (10<20 ));
+        System.out.println( !(3>2) );
+    }
+}
