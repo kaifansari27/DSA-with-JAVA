e863b98e0774b4c064a168b298392dbe159c22e0
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 14:12:14 +0530

    student is fail or pass

---
+++ 3_Conditional Statements/passORfell.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class passORfell {
+    public static void main(String[] args) {
+        int marks = 45;
+        String ReportCard = (marks >=33) ? "Pass" : "fail";
+        System.out.println(ReportCard);
+    }
+}
