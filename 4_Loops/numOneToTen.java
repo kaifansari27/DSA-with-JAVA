f35818f6f7f0a4ca4aa10316321a96f5e9222339
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 18:49:32 +0530

    print 1 to 10

---
+++ 4_Loops/numOneToTen.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class numOneToTen {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number <= 10) {
+            System.out.print(number + " ");
+            number++;
+            
+        }
+        System.out.println("Number is Printed (1-10)");
+    }
+}
