6b46f60696d9c7ffd10f8dc851a5446c4f214c30
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 13:56:46 +0530

    ternary operators

---
+++ 3_Conditional Statements/ternary.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class ternary {
+    public static void main(String[] args) {
+        int number = 27 ;
+        String type = ((number%2)==0)? "Even" : "Odd";
+        System.out.println(type);
+    }
+}
