54ded03b93bc7c0c8c260aca76c1690e3f61ce79
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 12:23:24 +0530

    else if condition
    esme agar if wala condition sahi hoga to baki ke skip ho jayega but if wala condition false hua to else if wala condition check hoga ......ye compiler ke computation time ko bachata hai

---
+++ 3_Conditional Statements/esleif.java
@@ -0,0 +1,17 @@
+ 
+public class esleif {
+    public static void main(String[] args) {
+        int age = 27;
+        if (age >= 18){
+            System.out.println("Adult:" + age);
+        }
+        else if (age > 13 && age < 18){
+            System.out.println("Teenager" + age);
+
+        }
+        else{
+            System.out.println("Bachh hai tu:" + age);
+        }
+    
+    }
+}
