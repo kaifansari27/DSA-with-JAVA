8de1e3233db4a6720d12644c691eb1e070f4aa8a
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-16 10:23:47 +0530

    Assingment operators

---
+++ 2_Operators in Java/AssignmentsOperators.java
@@ -0,0 +1,25 @@
+public class AssignmentsOperators {
+    public static void main(String[] args) {
+        int A = 10;
+        //A = A + 10;
+        A += 10;
+        int B = 20 ;
+        //B = B - 10
+        B -= 10;
+        int C = 30 ;
+        //C = C * 2 ;
+        C *= 2 ;
+        int D = 40;
+        //D = D / 4 ;
+        D /= 10;
+        int E = 50 ;
+        //E = E % 4;
+        E %= 4 ;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+        System.out.println(E);
+
+    }
+}
