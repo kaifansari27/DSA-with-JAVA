cb2c6caf04c438f504dc5dcb2fc472ab40fe1214
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-17 10:00:59 +0530

    Squar patter

---
+++ 4_Loops/SquarPattern.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SquarPattern {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1 ; i <=n ; i++){
+            System.out.println("* * * *");
+        }
+    }
+}
