1ab8a48eba1246c2156191bd18609fd530e06579
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-19 12:10:09 +0530

    best way to print Number is Prime or Not

---
+++ 5_Function or Methods/PrimeOrNot.java
@@ -0,0 +1,18 @@
+public class PrimeOrNot {
+    public static boolean isPrime(int n){
+        //Corner case
+        //2
+        if (n == 2){
+            return true ;
+        }
+        for (int i = 2 ; i <= n-1 ; i++){
+            if(n % 2 == 0){
+                return false ;
+            }
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(27));
+    }
+}
