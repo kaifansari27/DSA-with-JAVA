a460297f39d3a476a0f326c13dd89bb096a4e606
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-20 17:20:40 +0530

    when colonm is not fixed in Arrays

---
+++ 6_Arrays And ArrayList/colNotFixedSize.java
@@ -0,0 +1,18 @@
+
+public class colNotFixedSize {
+    public static void main(String[] args) {
+        int [][] arr = {
+            {1, 2, 3,4},
+            {6,7},
+            {8,9,5}
+        };
+        for (int row = 0 ; row < arr.length ; row ++){
+            for (int col = 0 ; col < arr[row].length ; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+           
+        }
+        System.out.println("Your Array is Printed:");
+    }
+}
