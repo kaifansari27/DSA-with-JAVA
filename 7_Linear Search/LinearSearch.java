4373ef2b2682dc0f4c2b3aa1eb8165ecc29bcbe6
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-21 14:16:48 +0530

    Linear Search

---
+++ 7_Linear Search/LinearSearch.java
@@ -0,0 +1,23 @@
+
+public class LinearSearch {
+    public static void main(String[] args) {
+        int [] nums = {23, 45, 56, 54, 65, 27, 24, 98, 25};
+        int  target = 27 ;
+        int ans = linearSearch(nums , target);
+        System.out.println(ans);
+
+    }
+    public static int linearSearch(int[] arr ,int target){
+        if (arr.length == 0){
+            return -1;
+        }
+        for(int i = 0; i < arr.length ; i++){
+            // check for element at every index 
+            int element = arr[i];
+            if (element == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
