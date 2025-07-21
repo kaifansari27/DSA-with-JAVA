ec6a0a36cf21f5de0e178e5ef23027efb25e198e
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-21 15:13:08 +0530

    Search in String

---
+++ 7_Linear Search/SearchInString.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+public class SearchInString {
+    public static void main(String[] args) {
+        String name = "Kaif";
+        char target = 'i';
+        System.out.println(search(name , target));
+    }
+    static boolean search (String str, char target){
+        if (str.length() == 0){
+            return false;
+        }
+        for (int i = 0 ; i < str.length() ; i++ ){
+
+            if (target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
