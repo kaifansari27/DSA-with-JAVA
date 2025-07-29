6ac20fccc6f18ed23f7e7358542355a634f730ff
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-29 15:00:27 +0530

    Pretty Printing

---
+++ 13_Strings and Strings Builder/PrettyPrinting.java
@@ -0,0 +1,9 @@
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 2728.232342f;
+        System.out.printf("Formatted number is %.2f\n" , a);
+        System.out.printf("pi: %.3f\n" , Math.PI);
+        System.out.printf("hello My name is %s and I am a %s person\n" , "Kaif" , "Cool");
+    }
+}
