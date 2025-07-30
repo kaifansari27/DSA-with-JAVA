c85cbc30fba533374233b052d4c11755e6029f1f
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-07-30 16:27:18 +0530

    Example of Recursion

---
+++ 15_Recursion/Message.java
@@ -0,0 +1,26 @@
+
+public class Message {
+    public static void main(String[] args) {
+        message();
+    }
+    static void message(){
+        System.out.println("Kaif Ansari");
+        message1();
+    }
+    static void message1(){
+        System.out.println("Kaif Ansari");
+        message2();
+    }
+    static void message2(){
+        System.out.println("Kaif Ansari");
+        message3();
+    }
+    static void message3(){
+        System.out.println("Kaif Ansari");
+        message4();
+    }
+    static void message4(){
+        System.out.println("Kaif Ansari");
+    }
+
+}
