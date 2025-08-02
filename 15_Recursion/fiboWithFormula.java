5e919841ee278e800a6c4ec788029192556bc42e
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-02 17:49:43 +0530

    Fibbonacci with Formula in recursion

--- 15_Recursion/fiboWithFormula.java
+++ 15_Recursion/fiboWithFormula.java
@@ -1,12 +1,13 @@
 public class fiboWithFormula {
     public static void main(String[] args) {
-        for(int i = 0 ; i < 11 ; i++ ){
+        /*for(int i = 0 ; i < 11 ; i++ ){
             System.out.print(fiboFormula(i) + " ");
-        }
+        }*/
+        System.out.println(fiboFormula(50));
         
     }
     static int fiboFormula(int n ){
-        return(int)((Math.pow(((1 + Math.sqrt(5)) / 2), n) - Math.pow(((1- Math.sqrt(5)) / 2), n)) /  Math.sqrt(5)) ;
+        return(int)(Math.pow(((1 + Math.sqrt(5)) / 2), n) /  Math.sqrt(5)) ;
     }
     static int fib(int n ){
         if (n < 2){
