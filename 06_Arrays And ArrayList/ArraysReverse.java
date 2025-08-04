15b946e485471fea62eca6a384251e26c5099955
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-04 19:52:27 +0530

    Reverse of the Array

---
+++ 06_Arrays And ArrayList/ArraysReverse.java
@@ -0,0 +1,10 @@
+
+public class ArraysReverse {
+    public static void main(String[] args) {
+        int[] arr = { 1, 3, 34, 32, 45, 22, 4 };
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
