b61ecbbd3b25ade6b468264142e91c46b8ea520a
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-05 10:30:03 +0530

    Pascle Triangle

---
+++ 16_Bitwise Operators(Bit Manuplation)/PascleTriangle.java
@@ -0,0 +1,7 @@
+public class PascleTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = (int)Math.pow(2 , (n-1));
+        System.out.println(ans);
+    }
+}
