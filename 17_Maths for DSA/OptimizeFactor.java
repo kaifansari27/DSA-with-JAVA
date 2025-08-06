78430aa5b62f95b48fde733df21e96004ea1fa31
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-06 16:23:38 +0530

    Optimize code for Factor

---
+++ 17_Maths for DSA/OptimizeFactor.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class OptimizeFactor {
+    public static void main(String[] args) {
+        
+        factor3(20);
+    }
+
+    // Brute force O(n)
+    static void factor1(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Optimized O(sqrt(n)), but unsorted output
+    static void factor2(int n) {
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " " + (n / i) + " ");
+                }
+            }
+        }
+    }
+
+    // Optimized and Sorted Output O(sqrt(n))
+    static void factor3(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                if (n / i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n / i);
+                }
+            }
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
