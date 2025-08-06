3c150a15f7d3b001421967df78fb9491ee90e4e6
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-06 14:47:38 +0530

    Newton Raphson  Method to find Sqrt root

---
+++ 17_Maths for DSA/NewtonSqaurRoot.java
@@ -0,0 +1,18 @@
+public class NewtonSqaurRoot {
+    public static void main(String[] args) {
+        System.out.println(sqrt(40));
+    }
+    public static double sqrt(double n ){
+        double x = n ; 
+        double root;
+        while (true) {  // if mera guess true ho gya toh...
+            root = 0.5 * ( x + (n / x));   //-----^
+            // for minimizing error...
+            if(Math.abs(root - x ) < 0.5){
+                break; 
+            }
+            x = root ;
+        }
+        return root ;
+    }
+}
