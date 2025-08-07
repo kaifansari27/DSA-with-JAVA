f2704440de90437078727fdb16254c51806d5a4a
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-07 10:34:26 +0530

    Finding GCD of Any Two Number

---
+++ 17_Maths for DSA/GCD_LCM.java
@@ -0,0 +1,12 @@
+public class GCD_LCM {
+    public static void main(String[] args) {
+        System.out.println("Your GCD is:");
+        System.out.println(gcd(4, 9));
+    }
+    public static int gcd(int  a , int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd( b % a , a);
+    }
+}
