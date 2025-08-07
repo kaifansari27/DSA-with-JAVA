4b88a5c132d37572b232ef0bebdcbd907e576607
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-07 17:49:58 +0530

    Finding Reverse of the Number

--- 18_ Recursio Leval 1 Question/ReverseOfNumber.java
+++ 18_ Recursio Leval 1 Question/ReverseOfNumber.java
@@ -11,9 +11,21 @@ public class ReverseOfNumber {
         rev1(n / 10);
     }
 
+    static int rev2(int n) {
+        int digit = (int) (Math.log10(n)) + 1; // we using this for count length of the digit
+        return helper(n, digit);
+    }
+
+    public static int helper(int n, int digit) {
+        if (n % 10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int) (Math.pow(10, digit - 1)) + helper(n/10, digit - 1);
+    }
+
     public static void main(String[] args) {
-        rev1(282769);
-        System.out.print(sum);
+        System.out.println(rev2(8734));
     }
 
 }
