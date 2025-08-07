ea5075252a527e7e9aa5db393488854dbc7595a5
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-07 15:17:43 +0530

    Finding Sum of the Digit

---
+++ 18_ Recursio Leval 1 Question/SumOfDigit.java
@@ -0,0 +1,25 @@
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        System.out.println(sumOfDigit(2728));
+    }
+    static int sumOfDigit(int n ){
+        if ( n == 0){
+            return n;
+        }
+        return (sumOfDigit(n/10) + (n %10 ));
+    }
+}
+
+/*
+       how it Internaly working 
+      
+       8 tumhara remaider hai... matlab last digit
+      
+      = 8 + sumOfDigit(272)
+      = 8 + 2 + sumOfDigit(27)
+      = 8 + 2 + 7 + sumOfDigit(2)
+      = 8 + 2 + 7 + 2 + sumOfDigit(0)
+      = 8 + 2 + 7 + 2 + 0 = 19 ✅
+
+ */
