fc6246d69af493275197f80ab27f436c04b2252d
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-08 14:03:09 +0530

    Linear Search using recursion

--- 19_Recursion - Array Questions/LinearSearch.java
+++ 19_Recursion - Array Questions/LinearSearch.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int [] arr = {2 , 4, 6, 9, 3, 8};
+        int [] arr = {2 , 4, 6, 9, 3, 2, 4, 8};
         System.out.println(find(arr , 4 , 0));
         System.out.println(findIndex(arr , 4 , 0));
         System.out.println(findIndexFromLast(arr , 4, arr.length -1));
+        findAllIndex(arr, 4, 0);
+        System.out.println(list);
 
     }
 
@@ -31,5 +34,15 @@ public class LinearSearch {
             return index ;
         }
         return findIndexFromLast(arr, target, index - 1);
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int [] arr , int target , int index){
+        if(index == arr.length){
+            return ;
+        }
+        if(arr[index] == target ){
+            list.add(index);
+        }
+        findAllIndex(arr , target , index+1); // we can't return this becoz this is void type function
     }
 }
