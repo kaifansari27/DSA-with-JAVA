f48a0ffe903a76c20f94459b7932402a46175d5c
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-08 18:07:50 +0530

    Linear Search

--- 19_Recursion - Array Questions/LinearSearch.java
+++ 19_Recursion - Array Questions/LinearSearch.java
@@ -2,47 +2,62 @@ import java.util.ArrayList;
 
 public class LinearSearch {
     public static void main(String[] args) {
-        int [] arr = {2 , 4, 6, 9, 3, 2, 4, 8};
-        System.out.println(find(arr , 4 , 0));
-        System.out.println(findIndex(arr , 4 , 0));
-        System.out.println(findIndexFromLast(arr , 4, arr.length -1));
+        int[] arr = { 2, 4, 6, 9, 3, 2, 4, 8 };
+        System.out.println(find(arr, 4, 0));
+        System.out.println(findIndex(arr, 4, 0));
+        System.out.println(findIndexFromLast(arr, 4, arr.length - 1));
         findAllIndex(arr, 4, 0);
         System.out.println(list);
+        System.out.println(findAllIndexWithAL(arr, 4, 0, new ArrayList<>()));
 
     }
 
     static boolean find(int[] arr, int target, int index) {
-        if (index == arr.length ) {
+        if (index == arr.length) {
             return false;
         }
-        return arr[index] == target || find(arr , target , index + 1);
+        return arr[index] == target || find(arr, target, index + 1);
     }
-    static int findIndex(int [] arr , int target , int index ){
-        if(index == arr.length){
+
+    static int findIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
             return -1;
         }
-        if(arr[index]== target){
-            return index ;
+        if (arr[index] == target) {
+            return index;
         }
-        return findIndex(arr, target, index+ 1);
+        return findIndex(arr, target, index + 1);
     }
-    static int findIndexFromLast(int [] arr , int target , int index ){
-        if(index == -1){
+
+    static int findIndexFromLast(int[] arr, int target, int index) {
+        if (index == -1) {
             return -1;
         }
-        if(arr[index]== target){
-            return index ;
+        if (arr[index] == target) {
+            return index;
         }
         return findIndexFromLast(arr, target, index - 1);
     }
+
     static ArrayList<Integer> list = new ArrayList<>();
-    static void findAllIndex(int [] arr , int target , int index){
-        if(index == arr.length){
-            return ;
+
+    static void findAllIndex(int[] arr, int target, int index) {
+        if (index == arr.length) {
+            return;
         }
-        if(arr[index] == target ){
+        if (arr[index] == target) {
             list.add(index);
         }
-        findAllIndex(arr , target , index+1); // we can't return this becoz this is void type function
+        findAllIndex(arr, target, index + 1); // we can't return this becoz this is void type function
+    }
+
+    static ArrayList<Integer> findAllIndexWithAL(int[] arr, int target, int index, ArrayList<Integer> list) {
+        if (index == arr.length) {
+            return list;
+        }
+        if (arr[index] == target) {
+            list.add(index);
+        }
+        return findAllIndexWithAL(arr, target, index + 1, list);
     }
 }
