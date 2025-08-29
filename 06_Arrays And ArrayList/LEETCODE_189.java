e58e0e5c5fbcaf584e19fae7394a8ffe0e4b9f49
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-29 23:25:09 +0530

    LEETCODE_189-(Rotate Array)

---
+++ 06_Arrays And ArrayList/LEETCODE_189.java
@@ -0,0 +1,19 @@
+public class LEETCODE_189 {
+    public void  rotate(int [] nums, int k ){
+        int n = nums.length;
+        k = k % n;             // Avoiding Unnecessary Rotation
+        reverse(nums , 0 , n -1 );
+        reverse(nums , 0 , k );
+        reverse(nums , k , n - 1);
+    }
+    public void reverse(int [] nums , int start , int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[start] = temp;
+            start ++;
+            end --;
+        }
+    }
+    
+}
