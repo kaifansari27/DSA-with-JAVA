275094c4c8d76c52a7684d6c742d431e18190968
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-08-31 00:30:53 +0530

    LEETCODE_ Code Testcase Test Result Test Result 238. Product of Array Except Self

--- 06_Arrays And ArrayList/LEETCODE_238.java
+++ 06_Arrays And ArrayList/LEETCODE_238.java
@@ -1,23 +1,41 @@
 public class LEETCODE_238 {
-    public static void main(String[] args) {
-        
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        // Step 1: Prefix products in ans[]
+        ans[0] = 1;
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Multiply with suffix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * suffix;
+            suffix *= nums[i];
+        }
+
+        return ans;
     }
-    
-    /* 
-    Brute Force Aproach...
+
+    /*
+    Brute Force Apraoch...
 
     public int[] productExceptSelf(int[] nums) {
-        int n = nums.length ;
-        int [] ans = new int [n];
-        for(int i = 0 ; i < n ; i++){
+        int n = nums.length;
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
             ans[i] = 1;
-            for(int j = 0 ; j < n ; j++ ){
-                if( i != j){
-                    ans [i] *= nums[j];
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    ans[i] *= nums[j];
                 }
             }
         }
         return ans;
-        
-    }*/
+
+    }
+    */
+
 }
