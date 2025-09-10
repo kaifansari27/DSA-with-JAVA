5d229aa453099bfc4421c9baa4ae1ffc06d8dd87
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-09-10 14:16:36 +0530

    LEETCODE_167. Two Sum II - Input Array Is Sorted

---
+++ 08_Binary Search/LEETCODE_167.java
@@ -0,0 +1,21 @@
+public class LEETCODE_167{
+    public static void main(String[]args){
+
+    }
+    public int towSum(int [] number , int target){
+        int left = 0 ;
+        int right number.length;
+
+        while(left < rigth){
+            int sum = number[left] + number[right];
+            if(sum == target){
+                return new int [] {left+1 , right + 1};
+            } else if (sum < target){
+                left ++;
+            } else{
+                right --;
+            }
+        }
+        return new int [] {-1 , -1};
+    }
+}
