37c55606e78a3cbeb91cc777ebb74af124afb69d
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-09-11 23:27:24 +0530

    LEETCODE - 167 – Two Sum II

--- 08_Binary Search/LEETCODE_167.java
+++ 08_Binary Search/LEETCODE_167.java
@@ -6,7 +6,7 @@ public class LEETCODE_167{
         int left = 0 ;
         int right number.length;
 
-        while(left < rigth){
+        while(left < right){
             int sum = number[left] + number[right];
             if(sum == target){
                 return new int [] {left+1 , right + 1};
