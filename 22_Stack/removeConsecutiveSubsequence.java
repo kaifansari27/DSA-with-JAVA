75e40e0e10fca4eac82f253cb80b3eb4190bdb58
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-10-13 03:25:26 +0530

    Remove Consecutive Subsequence

---
+++ 22_Stack/removeConsecutiveSubsequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class removeConsecutiveSubsequence {
+    public static int [] remove( int [] arr){
+        int n = arr.length;
+        Stack <Integer> st = new Stack<>();
+        for( int i = 0 ; i < n ; i++){
+            if(st.size() == 0 || st.peek() != arr[i]){
+                st.push(arr[i]);
+            }else if(arr[i] == st.peek()){
+                if( i == n-1  || arr[i] != arr[i+1]) st.pop();
+            }
+        }
+        // now put the output(Stack's remaining Elememts) in arrr
+        int [] res = new int [st.size()];
+        int m = res.length ; // find the size
+        // print in reverse for storing in array;
+        for (int i = m-1 ; i >= 0 ; i--){
+            res[i] = st.pop();
+        }
+        return res;
+
+
+    }
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 10, 10, 10, 2, 2, 4, 4, 4,5, 7, 7, 7 };
+        int [] res = remove(arr);
+        for(int i = 0 ; i < res.length ; i++){
+            System.out.print(res[i] + " ");
+        }
+    }
+}
