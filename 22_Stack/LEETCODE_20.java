fac0c3a5b2dd6fff04dd3d2ad06876c3347f051c
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-10-12 22:44:55 +0530

    LEETCODE_20. Valid Parentheses

---
+++ 22_Stack/LEETCODE_20.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class LEETCODE_20 {
+    public static void main(String[] args) {
+        
+    }
+    /*public boolean ValidParanthesis(String s){
+        while (true){
+            if(s.contains("()")){
+                s.replace("()" , " ");
+            }else if (s.contains("{}")){
+                s.replace("{}", " ");
+            }
+            else if (s.contains("[]")){
+                s.replace("[]" , " ");
+            }
+            else {
+                return s.isEmpty();
+            }
+        }
+    }*/
+
+    // 2nd Method using Stack
+    
+    public boolean isValid(String s){
+        Stack <Character> st = new Stack ;
+
+        int n = s.length ;
+
+        for (int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i);
+            if( ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            } else{
+                if(st.size() == 0) return false;
+
+                char top = st.peek();
+
+                if((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')){
+                    st.pop();
+                } else{
+                    return false ;
+                }
+
+            }  
+        }
+        return st.isEmpty();
+    }
+}
