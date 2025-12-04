5ac0b763cc1b30b45aefd30ecacecda2d8cd981c
Kaif-Ansari-27 <kaifansari.students@gmail.com>
2025-12-04 08:50:22 +0530

    Finding Reverse Of the Number Using Recursion

---
+++ 15_Recursion/ReverseOfNumber.java
@@ -0,0 +1,10 @@
+public class ReverseOfNumber {
+    public static int reverse(int n , int r){
+        if(n ==0 ) return r ;
+        return reverse(n/10, r*10+n%10);
+    }
+    public static void main(String[]args){
+        int n = 2718;
+        System.out.println(reverse(n, 0));
+    }
+}
